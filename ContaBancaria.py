1344922a6c6468d244c342a642acb99639ae0aae
Wagratom <wagratom111@gmail.com>
2024-08-07 14:13:15 -0300

    feat: separates and organizes the code

---
+++ ContaBancaria.py
@@ -0,0 +1,34 @@
+# resolvi aceitar o valor 0
+class ContaBancaria :
+    def __init__(self, saldo_inicial=0):
+        if saldo_inicial < 0:
+            raise Exception("Invalid initial balance. Account not created.")
+        self.saldo = saldo_inicial
+
+    def depositar(self, valor):
+        if valor < 0:
+            raise Exception("Invalid value for deposit. Deposit not performed.")
+
+        self.saldo += valor
+        print(f"Depósito de R${valor} realizado. Novo saldo: R${self.saldo}")
+
+
+    def sacar(self, valor):
+        if valor < 0:
+            raise Exception("Invalid value for withdrawal. Withdrawal not performed.")
+        if self.saldo < valor:
+            raise Exception("Insufficient balance. Operation not performed.")
+
+        self.saldo -= valor
+        print(f"Saque de R${valor} realizado. Novo saldo: R${self.saldo}")
+
+    def transferir(self, destinatario, valor):
+
+        if valor < 0:
+            raise Exception("Cannot transfer negative values!")
+        if self.saldo < valor:
+            raise Exception("Cannot transfer more than the balance!")
+
+        self.saldo -= valor
+        destinatario.depositar(valor)
+        print(f"Transferencia de R$({valor}) realizada. Novo saldo: R${self.saldo}")
